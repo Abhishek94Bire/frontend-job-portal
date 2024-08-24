b8f3c11150f73be9086131fa42b132af75cb0866
Abhishek94Bire <salesagroprollp@gmail.com>
2024-08-24 14:39:31 +0530

    new commit

--- src/components/auth/Signup.jsx
+++ src/components/auth/Signup.jsx
@@ -80,7 +80,7 @@ const Signup = () => {
                             value={input.fullname}
                             name="fullname"
                             onChange={changeEventHandler}
-                            placeholder="patel"
+                            placeholder="Abhishek Bire"
                         />
                     </div>
                     <div className='my-2'>
@@ -90,7 +90,7 @@ const Signup = () => {
                             value={input.email}
                             name="email"
                             onChange={changeEventHandler}
-                            placeholder="patel@gmail.com"
+                            placeholder="Abhibire94@gmail.com"
                         />
                     </div>
                     <div className='my-2'>
@@ -110,7 +110,7 @@ const Signup = () => {
                             value={input.password}
                             name="password"
                             onChange={changeEventHandler}
-                            placeholder="patel@gmail.com"
+                            placeholder="abhibire94@gmail.com"
                         />
                     </div>
                     <div className='flex items-center justify-between'>
